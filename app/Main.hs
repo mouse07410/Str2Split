18bc471cb7b2ebe1dfb91caa9e3444cc0b7bc128
Uri Blumenthal <uri@ll.mit.edu>
2020-02-21 11:30:23 -0500

    Added type signatures to imported functions

--- app/Main.hs
+++ app/Main.hs
@@ -4,12 +4,12 @@ import Lib (chunker, splitLess, splitMore, splitStr)
 
 main :: IO ()
 main = do
-  let s = "2d4fda81c8ce783asomeFunc"
+  let s = "2d4fda81c8ce783asomeFunc" :: String
   putStrLn "Testing splitStr with complicated 'foldr'"
-  print $ splitStr 3 s
+  print (splitStr 3 s :: String)
   putStrLn "Testing my 'chunker' that uses 'unfoldr'"
-  print $ chunker 2 s
+  print (chunker 2 s :: [String])
   putStrLn "Testing \"splitLess\" that uses 'unfoldr' and 'intercalate'"
-  print $ splitLess 4 s
+  print (splitLess 4 s :: String)
   putStrLn "Testing \"splitMore\" that uses 'unfoldr' and 'unwords'"
-  print $ splitMore 2 s
+  print (splitMore 2 s :: String)
