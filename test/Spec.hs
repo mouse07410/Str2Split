02340d5665c23505e01e045f4832a83f81a6b870
Mouse <mouse008@gmail.com>
2020-04-08 18:02:43 -0400

    Formatter insists on re-ordering imports

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,16 +1,15 @@
+import Control.Exception (evaluate)
 import Test.Hspec
 import Test.QuickCheck
-import Control.Exception (evaluate)
 
 main :: IO ()
 -- main = putStrLn "Test suite not yet implemented"
-main = hspec $ do
+main =
+  hspec $ do
     describe "Prelude.head" $ do
-        it "returns the first elem of a list" $ do
-            head [23 ..] `shouldBe` (23 :: Int)
-
-        it "returns the first element of an *arbitrary* list" $
-            property $ \x xs -> head (x:xs) == (x :: Int)
-      
-        it "throws an exception if used with an empty list" $ do
-            evaluate (head []) `shouldThrow` anyException
+      it "returns the first elem of a list" $ do
+        head [23 ..] `shouldBe` (23 :: Int)
+      it "returns the first element of an *arbitrary* list" $
+        property $ \x xs -> head (x : xs) == (x :: Int)
+      it "throws an exception if used with an empty list" $ do
+        evaluate (head []) `shouldThrow` anyException
